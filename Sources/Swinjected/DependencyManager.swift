b3b537e59ba5a19efe6703ff9861873740b78f16
Julian Manke <julian.manke91@gmail.com>
2024-04-21 18:16:08 +0200

    makes classes public

--- Sources/Swinjected/DependencyManager.swift
+++ Sources/Swinjected/DependencyManager.swift
@@ -9,12 +9,12 @@ import Foundation
 import SwiftData
 
 /// Collects all dependencies. Define all dependencies to be used across the app in this class.
-class DependencyManager {
+public class DependencyManager {
     private let storage = DependencyStorage.shared
     
     /// Adds dependency to be used in the app. Creare instances within this method.
     /// Call this method for every dependency you want to use in your app.
-    func addDependency<T>(_ dependency: T, for protocolType: Any.Type) {
+    public func addDependency<T>(_ dependency: T, for protocolType: Any.Type) {
         storage.add(dependency, for: protocolType)
     }
 }
--- Sources/Swinjected/Injected.swift
+++ Sources/Swinjected/Injected.swift
@@ -12,30 +12,11 @@ import Foundation
 @propertyWrapper
 /// API for dependency resolution. Example usage:
 /// @Injected var propertyName PropertyClass PropertyType.self
-struct Injected<T> {
+public struct Injected<T> {
     let wrappedValue: T
 
-    init() {
+    public init() {
         wrappedValue = DependencyStorage.shared.resolve()
-    }
-}
-
-@propertyWrapper
-struct LazyInjected<T> {
-    private var value: T?
-    private let resolver: () -> T
-
-    init() {
-        self.resolver = { DependencyStorage.shared.resolve() }
-    }
-
-    var wrappedValue: T {
-        mutating get {
-            if value == nil {
-                value = resolver()
-            }
-            return value!
-        }
     }
 }
 

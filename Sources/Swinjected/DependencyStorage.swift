c73faf992abe793a8ba5e0e2589c05225cea0c57
JManke91 <Julian.Manke91@gmail.com>
2024-05-13 17:46:56 +0200

    revert changes

--- Sources/Swinjected/DependencyStorage.swift
+++ Sources/Swinjected/DependencyStorage.swift
@@ -19,7 +19,7 @@ public class DependencyStorage {
     private var storage = [String: Any]()
 
     func add<T>(_ dependency: T, for type: Any.Type) {
-        let id = "\(type.description)"
+        let id = String(description: type)
         storage[id] = dependency
     }
 
